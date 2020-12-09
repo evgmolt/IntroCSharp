94da911ac2c52b4d1d255d718c35a291737e7b23
evgmolt <evg.molt@gmail.com>
2020-12-09 09:20:01 +0400

    Home work 1. Console application

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            DateTime datetoday = DateTime.Now;
+            Console.WriteLine($"Hello, {name}, today "+datetoday.ToShortDateString());
+            Console.ReadKey();
+        }
+    }
+}
